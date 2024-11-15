00d414395013c3a9507ba6509d951e7ae746009b
Erik Kiel <kielerik@informatik.hu-berlin.de>
2024-11-15 22:36:35 +0100

    Inital Commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# BahnBonus Punkteverfall Rechner
+
+In diesem Repository befindet sich ein Python-Skript, das Transaktionen von BahnBonus Punkten aus einer JSON-Datei (`transaktionen.json`) verarbeitet und die zu verfallenden Bonuspunkte pro Quartal für die drei Gültigkeitsjahre berechnet. Das Programm fragt nach dem aktuellen Bonuspunktestand (um anteilige Prämienausgaben zu berechnen) und gibt die Ergebnisse in der Konsole aus.
+
+Die Idee kam daher, dass App und Webseite immer nur den Verfall zum Ende des aktuellen Quartals anzeigen. Wann jedoch mit dem Verfall von Punkten zu rechnen ist, ist nicht ersichtlich. Dieses Skript soll hierbei helfen.
+
+## Disclaimer
+
+Dieses Skript ist als spontane Idee entstanden und nicht offiziell von der Deutschen Bahn. Es wird keine Garantie für die Richtigkeit der Berechnungen übernommen. Tests wurden mit den eigenen Transaktionen durchgeführt, jedoch kann es zu Fehlern kommen. Bei Unstimmigkeiten sehr gerne ein Issue erstellen oder einen Pull Request einreichen.
+
+## Voraussetzungen
+
+- Python 3.8 or higher
+
+## Installation
+
+1. Dieses Repository klonen:
+    ```sh
+    git clone https://github.com/ErKi1998/BahnBonus-Punkteverfall-Rechner.git
+    cd BahnBonus-Punkteverfall-Rechner
+    ```
+
+2. Sicherstellen, dass mindestens Python Version 3.8 installiert ist. Dazu kann folgender Befehl genutzt werden:
+    ```sh
+    python --version
+    ```
+   
+## Erhalten der Transaktionsdaten
+Um die Transaktionsdaten zu erhalten, die Seite [bahn.de/buchung/kundenkonto/bahnbonus](https://www.bahn.de/buchung/kundenkonto/bahnbonus) mit aktivierter Netzwerkanalyse besuchen und einloggen. Dort die Datei `transaktionen` auswählen und die Antwort als Datei mit dem Namen `transaktionen.json` speichern:
+
+![Network Analysis Tool](Images/ScreenshotBahnAccount.png)
+
+## Verwendung
+
+1. Die Datei `transaktionen.json` in das Verzeichnis des Projekts kopieren. Hierbei die im Repository vorhandene Beispiel-Datei überschreiben.
+
+2. Das Skript ausführen und den aktuellen Bonuspunktestand als Argument übergeben:
+    ```sh
+    python bahnbonus-calculator.py <aktuelle_bahn_bonus_punkte>
+    ```
+
+    Alternativ kann das Skript auch ohne Argumente ausgeführt werden. In diesem Fall wird bei der Ausführung nach dem aktuellen Bonuspunktestand gefragt:
+    ```sh
+    python bahnbonus-calculator.py
+    ```
+
+## Beispiel
+Ruft man das Skript mit dem Argument `2266` und der im Repository als Beispiel vorhandenen `transaktionen.json`-Datei auf:
+   ```sh
+   python main.py 2266
+   ```
+Erhält man folgende Ausgabe:
+```
+Pro Quartal gesammelte Punkte:
+Q4 2024: 80
+Q3 2024: 542
+Q2 2024: 434
+Q1 2024: 32
+Q4 2023: 0
+Q3 2023: 73
+Q2 2023: 359
+Q1 2023: 0
+Q4 2022: 276
+Q3 2022: 111
+Q2 2022: 380
+Q1 2022: 40
+Q4 2021: 0
+
+Summe der aus den Transaktionen berechneten Punkte: 2327
+Aktuelle Anzahl der BahnBonus Punkte: 2266
+
+Es gibt einen Unterschied von 61 Punkten. Dies stammt vrsl. von Premienausgaben. Dies wird nun berücksichtigt.
+
+In Folgenden Quartalen laufen die Punkte ab:
+Q4 2024: 0
+Q1 2025: 0
+Q2 2025: 359
+Q3 2025: 111
+Q4 2025: 276
+Q1 2026: 0
+Q2 2026: 359
+Q3 2026: 73
+Q4 2026: 0
+Q1 2027: 32
+Q2 2027: 434
+Q3 2027: 542
+Q4 2027: 80
+```
---
+++ bahnbonus-calculator.py
@@ -0,0 +1,96 @@
+import json
+import sys
+from datetime import datetime, timedelta
+
+def get_dict_of_last_quarters(num_last_quarters):
+    current_date = datetime.now()
+    quarters = {}
+
+    for i in range(num_last_quarters):
+        quarter = (current_date.month - 1) // 3 + 1
+        year = current_date.year
+        quarters[f'Q{quarter} {year}'] = 0
+        current_date -= timedelta(days=90)  # Approximate 3 months back
+
+    return quarters
+
+def get_quarter(date_str):
+    # Parse the date string
+    date = datetime.strptime(date_str, '%Y-%m-%d')
+
+    # Determine the quarter
+    quarter = (date.month - 1) // 3 + 1
+
+    # Format the result as "QX YYYY"
+    return f'Q{quarter} {date.year}'
+
+
+def main():
+    # Python Version 3.8 or higher is required for this script to work (order of dictionary items is guaranteed)
+    # check if the python version is 3.8 or higher
+    if sys.version_info < (3, 8):
+        print('Python 3.8 or höher wird benötigt.')
+        sys.exit(1)
+
+    # Read JSON content from file
+    with open('transaktionen.json', 'r') as file:
+        json_input = file.read()
+
+    # Parse JSON content
+    data = json.loads(json_input)
+
+    # Get the current bonus points from stdin or pass it as an argument
+    if len(sys.argv) == 2:
+        current_bonus_points = int(sys.argv[1])
+    else:
+        current_bonus_points = int(input('Bitte die aktuelle Anzahl der BahnBonus Punkte angeben: '))
+
+    # a dictionary to store the total points per quarter
+    bonus_points_per_quarter = get_dict_of_last_quarters(13)
+
+    for transaction in data['transactions']:
+        # Continue if the transaction is not earmarked (a future transaction) or a points expiration
+        if not transaction['earmarked'] and not transaction['transactionCategory'] == 'Punkteverfall' and not transaction['transactionCategory'] == 'Prämienbestellung':
+            quarter_of_transaction = get_quarter(transaction['transactionDate'])
+            # Check if the quarter is in the dictionary, sometimes DB has transactions from before the last 13 quarters
+            if quarter_of_transaction in bonus_points_per_quarter:
+                bonus_points_per_quarter[quarter_of_transaction] += transaction['pointsBB']
+
+
+    # Summarize the points per quarter
+    sum_of_points = 0
+    print("\nPro Quartal gesammelte Punkte:")
+    for quarter, points in bonus_points_per_quarter.items():
+        sum_of_points += points
+        print(f'{quarter}: {points}')
+
+    # print total points and current bonus points
+    print(f'\nSumme der aus den Transaktionen berechneten Punkte: {sum_of_points}') # Sum of all points
+    print(f'Aktuelle Anzahl der BahnBonus Punkte: {current_bonus_points}') # Current bonus points
+
+    difference_in_points = sum_of_points - current_bonus_points
+
+    if difference_in_points > 0:
+        print(f'\nEs gibt einen Unterschied von {difference_in_points} Punkten. Dies stammt vrsl. von Premienausgaben. Dies wird nun berücksichtigt.')
+        for quarter, points in reversed(bonus_points_per_quarter.items()):
+            if points < difference_in_points:
+                bonus_points_per_quarter[quarter] = 0
+                difference_in_points -= points
+            else:
+                bonus_points_per_quarter[quarter] -= difference_in_points
+                break
+
+    # print the final result
+    print("\nIn Folgenden Quartalen laufen die Punkte ab:")
+
+    # Print the final points per quarter + 3 years
+    for quarter, points in reversed(bonus_points_per_quarter.items()):
+        # increase the quarter by 3 years
+        quarter = quarter.split()
+        quarter[1] = str(int(quarter[1]) + 3)
+        quarter = ' '.join(quarter)
+        print(f'{quarter}: {points}')
+
+
+if __name__ == '__main__':
+    main()
